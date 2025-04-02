456d934c9ab60c6810b178fc91c296b647e3b880
IgnacioIFGIne <ignacio.delafuente@ineco.com>
2025-04-02 13:55:07 +0200

    Add geolocation handling

--- frontend/src/app/registro-incidencia/registro-incidencia.component.ts
+++ frontend/src/app/registro-incidencia/registro-incidencia.component.ts
@@ -20,9 +20,52 @@ export class RegistroIncidenciaComponent {
 
   constructor(public dialogRef: MatDialogRef<RegistroIncidenciaComponent>, private inspectorService: InspectorService) {}
 
+
+  ngOnInit(): void {
+    console.log("ngOnInit ejecutado"); // Verifica que se ejecuta
+    this.obtenerUbicacion();
+
+  }
+
+  obtenerUbicacion() {
+    if (!navigator.geolocation) {
+      console.error("Geolocalización no está soportada en este navegador.");
+      return;
+    }else{
+      console.log("Geolocalización disponible en este navegador.");
+    }
+  
+    console.log("Intentando obtener ubicación...");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.incidencia.ubicacion = `${position.coords.latitude}, ${position.coords.longitude}`;
+        console.log("Ubicación obtenida:", this.incidencia.ubicacion);
+      },
+      (error) => {
+        console.error("Error obteniendo ubicación:", error);
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            console.error("El usuario denegó el permiso de geolocalización.");
+            break;
+          case error.POSITION_UNAVAILABLE:
+            console.error("Información de ubicación no disponible.");
+            break;
+          case error.TIMEOUT:
+            console.error("La solicitud de geolocalización expiró.");
+            break;
+          default:
+            console.error("Error desconocido al obtener ubicación.");
+        }
+      },
+      { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 } // Configuración avanzada
+    );
+  }
+
+
   registrarIncidencia(): void {
 
     console.log('Datos de la incidencia:', this.incidencia.elemento);
+    console.log("OK", "ubicacion", this.incidencia.ubicacion);
 
     this.inspectorService.registrarIncidencia(this.incidencia).subscribe(
       res => (res =="ok")?this.inciOk():alert("error al registrar la incidencia"))
